4c3778d6946986809737c6279105f30a66870e4e
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-11-30 11:39:21 +0700

    added static path

--- index.js
+++ index.js
@@ -31,12 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') app.use(cors());
 app.use('/api', studentRoute);
 
-// PORT
-const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
-  console.log(`Connected to port ${port}...`);
-})
-
 // 404 Error
 app.use((req, res, next) => {
   console.log('error 404')
@@ -48,4 +42,18 @@ app.use((err, req, res, next) => {
   console.error(err.message);
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
+})
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
+// PORT
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Connected to port ${port}...`);
 })
