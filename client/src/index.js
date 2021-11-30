a82d0d9ee5f044825d1487cd2879f7b1768229de
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-11-30 15:20:32 +0700

    change code back to original

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-import './index.css';
 import App from './App';
-import Debug from './Debug';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <App /> */}
-      <Debug />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
